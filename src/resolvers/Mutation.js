85e182c14429c90c517cdf3ff5d4f7de93795f87
educartoons <educartoons@gmail.com>
2019-10-03 17:23:34 -0500

    adding update item feature

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,13 +1,29 @@
 const Mutations = {
-  async createItem(parent, args, context, info){
-    const item = await context.db.mutation.createItem({
-      data: {
-        ...args
-      }
-    }, info)
-
-    console.log(item)
+  async createItem(parent, args, context, info) {
+    const item = await context.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
     return item;
+  },
+
+  updateItem(parent, args, context, info) {
+    const updates = { ...args };
+    delete updates.id;
+
+    return context.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
   }
 };
 
