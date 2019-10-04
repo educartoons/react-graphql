d09ddf650c320077389606e203a56a84986183f1
educartoons <educartoons@gmail.com>
2019-10-03 23:31:23 -0500

    Adding Cookie Middleware

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-require("dotenv").config({path: 'variables.env'});
+const cookieParser = require("cookie-parser");
+require("dotenv").config({ path: "variables.env" });
 
-const createServer = require('./createServer')
-const db = require('./db');
+const createServer = require("./createServer");
+const db = require("./db");
 
 const server = createServer();
 
+// Use expresss middleware to handle cookies JWT
 
-server.start({
-  cors: {
-    credentials: true,
-    origin: process.env.FRONTEND_URL,
+server.express.use(cookieParser());
+
+//
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
   },
-}, deets => {
-  console.log(`Server is now running on port http://localhost:${deets.port}`)
-})
+  deets => {
+    console.log(`Server is now running on port http://localhost:${deets.port}`);
+  }
+);
--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const Mutations = {
   async createItem(parent, args, context, info) {
     const item = await context.db.mutation.createItem(
@@ -31,6 +34,25 @@ const Mutations = {
     const item = await context.db.query.item({ where }, `{id title}`);
     // Check if
     return context.db.mutation.deleteItem({ where }, info);
+  },
+  async signUp(parent, args, context, info) {
+    args.email = args.email.toLowerCase();
+    // has their password
+    const password = await bcrypt.hash(args.password, 10);
+    // create user in the db
+    const user = await context.db.mutation.createUser(
+      { data: { ...args, password, permissions: { set: ["USER"] } } },
+      info
+    );
+    // create the JWT Token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // we set the jwt as a cokkie on the response
+    context.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365 // 1 Year cookie
+    });
+    // finally reeturn the user to the browser
+    return user;
   }
 };
 
