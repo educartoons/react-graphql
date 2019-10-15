a2ca0dc255753259c7fd17b55deb5196b291f8a1
educartoons <educartoons@gmail.com>
2019-10-15 14:15:59 -0500

    Added Sign Out Function

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -74,6 +74,10 @@ const Mutations = {
       maxAge: 1000 * 60 * 60 * 24 * 365
     });
     return user;
+  },
+  signOut(parent, args, context, info) {
+    context.response.clearCookie("token");
+    return { message: "Goodbye!" };
   }
 };
 
